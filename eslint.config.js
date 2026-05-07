148ae6e8cd265ee110d71bea2a4de8443daa81e8
zeshalondrag <sanyayt1337@gmail.com>
2026-05-07 18:47:29 +0300

    eslint 3.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
@@ -6,5 +5,13 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
   },
 ]);
